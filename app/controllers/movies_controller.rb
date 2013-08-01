8f586b06fdf6f53401c8a288338af5aca49e7dcc
Risa Batta <rbatta@gmail.com>
2013-07-31 22:31:29 -0700

    refactored code to make sorting work properly

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,7 @@ class MoviesController < ApplicationController
     @ratings_hash = { "G" => false, "PG" => false, "PG-13" => false, "R" => false }
     # if( nothing set ) { set all true } else { do mapping }
     
-    arr = []
+    
     
 =begin
   
@@ -33,43 +33,39 @@ if params has nothing and session has nothing
     view all movies (load all)
   
 =end
-    
+    p_rat = []
+    s_rat = []
+    p_rat = params[:ratings].map { |x,y| x } if params[:ratings]
+    s_rat = session[:ratings].map { |x,y| x } if session[:ratings]
+
+    debugger
     if params[:sort_by] && params[:ratings]
-      params[:ratings].map do |x,y|      # eg. { "G" => 1, "PG" => 1}
-        arr << Movie.order(params[:sort_by]).where('rating = ?', x)   # this returns an array of arrays (however many i've chosen)
-        @ratings_hash[x] = true
-      end
-      @all_ratings = Movie.all_ratings
-      @movies = arr.flatten
+      @movies = Movie.where("rating = '#{p_rat.join("' OR rating = '")}'").order(params[:sort_by])
+      p_rat.each { |x| @ratings_hash[x] = true }
       session[:sort_by] = params[:sort_by]
       session[:ratings] = params[:ratings]
       
     elsif params[:ratings] == nil && params[:sort_by] == nil
+      
       if session[:ratings] == nil && session[:sort_by] == nil
         @movies = Movie.all
-        @ratings_hash = { "G" => true, "PG" => true, "PG-13" => true, "R" => true }
+        @ratings_hash.update(@ratings_hash) { |x, y| y = true }
       else
-        session[:ratings].map do |x,y|      
-          arr << Movie.order(session[:sort_by]).where('rating = ?', x)   
-          @ratings_hash[x] = true
-        end
-        @all_ratings = Movie.all_ratings
-        @movies = arr.flatten
+        @movies = Movie.where("rating = '#{s_rat.join("' OR rating = '")}'").order(session[:sort_by])    
+        s_rat.each { |x| @ratings_hash[x] = true }
       end
 
     elsif params[:sort_by]
       @movies = Movie.order(params[:sort_by])
-      @ratings_hash = { "G" => true, "PG" => true, "PG-13" => true, "R" => true }
+      @ratings_hash.update(@ratings_hash) { |x, y| y = true }
       session[:sort_by] = params[:sort_by]
+      session.delete(:ratings)
 
     else
-      params[:ratings].map do |x,y|      
-        arr << Movie.order(params[:sort_by]).where('rating = ?', x)   
-        @ratings_hash[x] = true
-      end
-      @all_ratings = Movie.all_ratings
-      @movies = arr.flatten
+      @movies = Movie.where("rating = '#{p_rat.join("' OR rating = '")}'").order(params[:sort_by])
+      p_rat.each { |x| @ratings_hash[x] = true }
       session[:ratings] = params[:ratings]
+      session.delete(:sort_by)
 
     end
     
